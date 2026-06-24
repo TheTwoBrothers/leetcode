4cea3157268dde2a93381cb45acefd5ed4d33c20
Utkarsh Singh <utkarshsingh3690@gmail.com>
2026-06-24 18:47:29 +0530

    Time: 0 ms (100%), Space: 46.3 MB (45.02%) - LeetHub

--- 0810-chalkboard-xor-game/0810-chalkboard-xor-game.java
+++ 0810-chalkboard-xor-game/0810-chalkboard-xor-game.java
@@ -1,38 +1,10 @@
 class Solution {
     public boolean xorGame(int[] nums) {
-        int bit=0;
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            bit^=nums[i];
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        if(bit==0)
-           return true;
-        boolean ans=false;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            boolean found=false;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                int key = entry.getKey();
-                int value = entry.getValue();
+        int xor = 0;
 
-                if((bit^key)!=0)
-                {
-                    found=true;
-                    value--;
-                    ans=!ans;
-                    bit=bit^key;
-                    if(value==0)
-                      map.remove(key);
-                    else
-                      map.put(key,value);  
-                    break;  
-                }
-            }
-            if(!found)
-               return ans;
+        for (int num : nums) {
+            xor ^= num;
         }
-        return ans;   
+        return xor == 0 || nums.length % 2 == 0;
     }
 }
