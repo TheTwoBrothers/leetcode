5822a7a5e0777341d65f83747bd9bb24fd8ac2e3
Utkarsh Singh <utkarshsingh3690@gmail.com>
2026-06-28 22:09:19 +0530

    Time: 7 ms (95.42%), Space: 77.5 MB (41.25%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,19 +1,28 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
-        HashSet<Integer>set=new HashSet<>();
-        int key=1;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(set.contains(arr[i]))
-                 continue;
-            if(arr[i]>=key)
-            {
-                set.add(key);
-                key++;
-            }     
+        // Arrays.sort(arr);
+        // HashSet<Integer>set=new HashSet<>();
+        // int key=1;
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     if(set.contains(arr[i]))
+        //          continue;
+        //     if(arr[i]>=key)
+        //     {
+        //         set.add(key);
+        //         key++;
+        //     }     
 
+        // }
+        // return key-1;
+        Arrays.sort(arr);
+        int m=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>m){
+            m+=1;
+            }
         }
-        return key-1;
+        return m;
+        
     }
 }
