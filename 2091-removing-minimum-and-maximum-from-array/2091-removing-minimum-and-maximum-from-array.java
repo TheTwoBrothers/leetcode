c635bc4f3c6210bc1c9bed9b79c702da1e685862
Utkarsh Singh <utkarshsingh3690@gmail.com>
2026-08-30 14:59:21 +0530

    Time: 2 ms (100%), Space: 87 MB (8.73%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,64 +1,30 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
         int n=nums.length;
+        if(n<=2) return n;
+        int maxval=Integer.MIN_VALUE;
+        int minval=Integer.MAX_VALUE;
+        int minInd=-1;
+        int maxInd=-1;
         for(int i=0;i<n;i++)
         {
-            max=Math.max(max,nums[i]);
-            min=Math.min(min,nums[i]);
+            if(nums[i]<minval)
+            {
+                minval=nums[i];
+                minInd=i;
+            }
+            if(nums[i]>maxval)
+            {
+                maxval=nums[i];
+                maxInd=i;
+            }
         }
-        int ans=n;
-        boolean small=false;
-        boolean large=false;
+        int a=Math.min(minInd,maxInd);
+        int b =Math.max(minInd,maxInd);
+         int front=b+1;
+         int back=n-a;
+         int both=(a+1)+(n-b);
+         return Math.min(front,Math.min(back,both));
 
-        for(int i=0;i<n;i++)
-        {
-            if(small&&large)
-              {
-                 ans=Math.min(ans,i);
-                 break;
-              }
-            if(nums[i]==min)
-                small=true;
-            if(nums[i]==max)
-                large=true;          
-        }
-        small=false;
-        large=false;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(small&&large)
-              {
-                 ans=Math.min(ans,n-i-1);
-                 break;
-              }
-            if(nums[i]==min)
-                small=true;
-            if(nums[i]==max)
-                large=true;          
-        }
-        small=false;
-        large=false;
-        int l=0;
-        int r=n-1;
-       for(int i=0;i<n;i++)
-       {
-          if(nums[i]==min||nums[i]==max)
-          {
-             l=i+1;
-             break;
-          }  
-       }
-        for(int i=n-1;i>=0;i--)
-       {
-          if(nums[i]==min||nums[i]==max)
-          {
-             r=n-i;
-             break;
-          }  
-       }
-       ans=Math.min(ans,r+l);
-       return ans;
     }
 }
