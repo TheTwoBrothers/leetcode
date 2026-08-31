04dd1e1cfae32cc40d24bb548df74797b4a25e7e
Utkarsh Singh <utkarshsingh3690@gmail.com>
2026-08-31 07:16:08 +0530

    Time: 0 ms (100%), Space: 42 MB (70.12%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int bulbSwitch(int n) {
+    if(n==0)
+       return 0;    
+    int ans=0;
+
+    for(int i=1;i*i<=n;i++)
+    {
+         ans++;
+    }
+    return ans;
+    }
+}
