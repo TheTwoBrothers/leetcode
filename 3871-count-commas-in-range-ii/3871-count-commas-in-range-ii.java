24e1d7535033b0cd258494f393cd95241764e2d3
Utkarsh Singh <utkarshsingh3690@gmail.com>
2026-09-09 14:44:36 +0530

    Time: 1 ms (99.36%), Space: 42.8 MB (24.36%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,14 +1,9 @@
 class Solution {
-
-    long max(long a, long b) {
-        return a > b ? a : b;
-    }
-
     public long countCommas(long n) {
-        return max(0L, n - 999L)
-             + max(0L, n - 999999L)
-             + max(0L, n - 999999999L)
-             + max(0L, n - 999999999999L)
-             + max(0L,n-999999999999999L);
+        return Math.max(0L, n - 999L)
+     + Math.max(0L, n - 999999L)
+     + Math.max(0L, n - 999999999L)
+     + Math.max(0L, n - 999999999999L)
+     + Math.max(0L, n - 999999999999999L);
     }
 }
